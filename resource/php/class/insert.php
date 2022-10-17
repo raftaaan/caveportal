6be0019d44e23e10d2b89cd31384a460b760aabd
valencia-ealyza <112416901+valencia-ealyza@users.noreply.github.com>
2022-10-17 22:58:37 +0800

    Added Validation

--- resource/php/class/insert.php
+++ resource/php/class/insert.php
@@ -14,29 +14,53 @@ class insert extends config{
         $this->degree = $degree;
         $this->yearsGrad = $yearsGrad;
         $this->diploma = $diploma;
+        $ext = pathinfo($this->diploma['name'], PATHINFO_EXTENSION);
         $this->consent = $consent;
+        $form = pathinfo($this->consent['name'], PATHINFO_EXTENSION);
 
-        $ext = strtolower(pathinfo($this->diploma['name'], PATHINFO_EXTENSION));
-        $this->diploma['name'] = $this->lastName.".".$ext;
-        $storage = "resource/files/diploma_files/";
-        $this->certificate = $storage . $this->diploma['name'];
-        move_uploaded_file($this->diploma['tmp_name'], $this->certificate);
         
-        $form = strtolower(pathinfo($this->consent['name'], PATHINFO_EXTENSION));
-        $this->consent['name'] = $this->lastName.".".$form;
-        $storage = "resource/files/consent_forms/";
-        $this->letterForm = $storage . $this->consent['name'];
-        move_uploaded_file($this->consent['tmp_name'], $this->letterForm);
-        
-        if ($ext == ''){
-            $this->certificate = '';
-        }
+        if($this->firstName == ""){
+            alertMess("Entering first name is required!");
+        }else if(!ctype_alpha(str_replace(' ', '', $this->firstName))){
+            alertMess("Entered first name is not applicable!");
+        }else if($this->middleName == ""){
+            alertMess("Entering middle name is required!");
+        }else if(!ctype_alpha(str_replace(' ', '', $this->middleName))){
+            alertMess("Entered middle name is not applicable!");
+        }else if($this->lastName == ""){
+            alertMess("Entering last name is required!");
+        }else if(!ctype_alpha(str_replace(' ', '', $this->lastName))){
+            alertMess("Entered last name is not applicable!");
+        }else if($ext !== 'pdf' && $ext !== ''){
+            alertMess("Diploma must be a pdf file!");  
+        }else if($form !== 'pdf' && $form !== ''){
+            alertMess("Consent Letter must be a pdf file!");  
+        }else{
+            $ext = strtolower(pathinfo($this->diploma['name'], PATHINFO_EXTENSION));
+            $this->diploma['name'] = $this->lastName.".".$ext;
+            $storage = "resource/files/diploma_files/";
+            $this->certificate = $storage . $this->diploma['name'];
+            move_uploaded_file($this->diploma['tmp_name'], $this->certificate);
+            
+            $form = strtolower(pathinfo($this->consent['name'], PATHINFO_EXTENSION));
+            $this->consent['name'] = $this->lastName.".".$form;
+            $storage = "resource/files/consent_forms/";
+            $this->letterForm = $storage . $this->consent['name'];
+            move_uploaded_file($this->consent['tmp_name'], $this->letterForm);
+            
+            if ($ext == ''){
+                $this->certificate = '';
+            }
 
-        if ($form == '') {
-            $this->letterForm = '';
+            if ($form == '') {
+                $this->letterForm = '';
+            }
         }
+        
     }
-
+    public function alertMess($message){
+        echo "<script>alert('$message');</script>";
+    }
     public function insertVerification(){
         $config = new config();
         $con= $config->con();
@@ -44,13 +68,10 @@ class insert extends config{
         $data = $con->prepare($sql);
         
         if($data->execute()) {
-            $message = "Application Registered Successfully";
+            alertMess("Application Registered Successfully");
         } else {
-            $message = "Application Registered Unsuccessful";
+            alertMess("Application Registered Unsuccessful");
         }
-        echo "<script>alert('$message');</script>";
     }
-
-   
 
 }
